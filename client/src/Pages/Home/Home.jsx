d2841182df4317e4b26c7783da2fec63bb6971da
MeghaJibil@1013 <meghathambi97@gmail.com>
2024-04-29 10:30:37 +0530

    props type update

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Header from '../../Components/Header/Header.jsx';
+import PropTypes from 'prop-types';
+
 
 function Home() {
   const [value, setValue] = useState(0);
@@ -54,5 +56,11 @@ function CustomTabPanel(props) {
     </div>
   );
 }
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node.isRequired, // Define children prop as a node type and it's required
+  value: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 export default Home;
